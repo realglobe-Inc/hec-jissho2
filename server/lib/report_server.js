8695a0c1610d7535cfa48232fc2331cbf4dab8f3
FujiHaruka <fujiharuka@realglobe.jp>
2016-11-11 19:07:30 +0900

    hubはtestのときにredisを使わない

--- server/lib/report_server.js
+++ server/lib/report_server.js
@@ -7,15 +7,22 @@ const endpoints = require('./endpoints/report')
 // const { observer } = require('./helpers/observer')
 const observer = () => {}
 
-let reportServer = sugoHub({
-  endpoints,
-  storage: {
-    redis: {
-      url: env.redis.URL,
-      db: 1
+let config = {
+  endpoints
+}
+
+if (process.env.NODE_ENV !== 'test') {
+  Object.assign(config, {
+    storage: {
+      redis: {
+        url: env.redis.URL,
+        db: 1
+      }
     }
-  }
-})
+  })
+}
+
+let reportServer = sugoHub(config)
 
 Object.assign(reportServer, {
   observer
