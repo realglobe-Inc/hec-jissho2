552a9194936a6a0fb6a71a0d43a9f87648c870e0
FujiHaruka <fujiharuka@realglobe.jp>
2016-11-28 17:57:29 +0900

    メニュー画面

--- ui/js/entries/index.tsx
+++ ui/js/entries/index.tsx
@@ -2,14 +2,35 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import AppStyle from '../components/app_style'
 import Header from '../components/header'
+import { ApButton } from 'apeman-react-button'
 
 const rootElement = document.getElementById('site')
 
+let links = [
+  {
+    url: 'system.html',
+    text: 'システム画面'
+  },
+  {
+    url: 'photos.html',
+    text: '隊員用画面'
+  }
+]
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <div>
+    <div className='app'>
       <AppStyle/>
       <Header/>
+      <div className='menu-wrapper'>
+        {
+          links.map((link) => (
+            <div className='menu-button' key={link.url}>
+              <a href={link.url}>{link.text}</a>
+            </div>
+          ))
+        }
+      </div>
     </div>,
     rootElement
   )
